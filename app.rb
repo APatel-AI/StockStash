c6789028090c4b7269b64d4991d1f5a43e214117
Arpan Patel <113603155+APatel-AI@users.noreply.github.com>
2024-02-22 20:00:41 +0000

    added result form and updated css template

--- app.rb
+++ app.rb
@@ -6,8 +6,28 @@ get("/") do
 end
 
 
-get("/search") do
-  
-  api_url = "https://api.polygon.io/v2/reference/tickers/search?q=#{ticker_symbol}&apiKey=#{ENV["STOCK_API_KEY"]}"
+get("/get_stock_data") do
+
+  #query parameters
+  ticker_symbol = params[:ticker]
+  start_date = params[:start_date]
+  end_date = params[:end_date]
+
+
+  api_url = "https://api.polygon.io/v2/aggs/ticker/#{ticker_symbol}/range/1/day/#{start_date}/#{end_date}?adjusted=true&sort=asc&limit=120&apiKey=#{ENV["STOCK_API_KEY"]}"
   response = HTTP.get (api_url)
+
+
+  #200 ok response handler
+
+  if response.code == 200
+    @stock_data = JSON.parse(response.body)
+    erb(:results)
+
+  else
+    # error 401, 404, etc..
+    @error_message = "Error: #{response.code}, #{response.reason}"
+
+  end
+
 end
